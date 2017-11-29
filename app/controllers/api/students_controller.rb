b3d990bb68cd552f52954d9857fe7ac1ad1dd81a
smithwebtek <brad@smithwebtek.com>
2017-11-29 14:24:23 -0500

    students#create method working

--- app/controllers/api/students_controller.rb
+++ app/controllers/api/students_controller.rb
@@ -4,4 +4,19 @@ class Api::StudentsController < ApplicationController
     @students = Student.all
     render json: @students
   end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      render json: @student
+    else
+      render json: { errors: { message: 'student NOT created' }}
+    end
+  end
+  
+  private
+  def student_params
+    params.require(:student).permit(:name, :level)
+  end
+
 end
