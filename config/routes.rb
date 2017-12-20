b69657ac1ae42b47da784133463f63a64dd55501
smithwebtek <brad@smithwebtek.com>
2017-12-19 20:26:10 -0500

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root '/api/students'
+
   namespace :api do
     resources :teachers
     resources :students
@@ -7,4 +9,5 @@ Rails.application.routes.draw do
     resources :lessons
     resources :lesson_resources
   end
+
 end
