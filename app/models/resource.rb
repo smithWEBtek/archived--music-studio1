802b02b3080a8614f7b84a98cf76dd5ce3a3eb65
smithwebtek <brad@smithwebtek.com>
2017-12-19 22:27:11 -0500

    add teachers to lesson serializer

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,5 +2,5 @@ class Resource < ApplicationRecord
   has_many :lesson_resources
   has_many :lessons, through: :lesson_resources
   has_many :students, through: :lessons
-  
+  has_many :teachers, through: :lessons
 end
