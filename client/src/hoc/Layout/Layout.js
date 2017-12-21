3e323656d6213b1d3c1d3380752a5112183c2c34
smithwebtek <brad@smithwebtek.com>
2017-12-20 20:11:15 -0500

    add toggle buttons to show/hide students, teachers, resources, lessons

--- client/src/hoc/Layout/Layout.js
+++ client/src/hoc/Layout/Layout.js
@@ -8,7 +8,19 @@ import Resources from '../../components/Resources/Resources';
 import Lessons from '../../containers/Lessons/Lessons';
 
 class Layout extends Component {
-    
+  state = {
+    studentsVisible: false,
+    teachersVisible: false,
+    resourcesVisible: false,
+    lessonsVisible: false
+  }
+
+  handleShow = (event) => { 
+    this.setState({
+      [event.target.value]: !this.state[event.target.value]
+    })
+  }
+
   render() {
     return (
       <Aux>
@@ -16,10 +28,18 @@ class Layout extends Component {
           {this.props.children}
         </main>
         <div className={[classes.App, classes.container].join(' ')}>
-          <Students />
-          <Teachers />
-          <Resources />
-          <Lessons />
+          <button onClick={(event)=>this.handleShow(event)} value='studentsVisible'>ShowStudents</button>
+          <button onClick={(event)=>this.handleShow(event)} value='teachersVisible'>ShowTeachers</button>
+          <button onClick={(event)=>this.handleShow(event)} value='resourcesVisible'>ShowResources</button>
+          <button onClick={(event)=>this.handleShow(event)} value='lessonsVisible'>ShowLessons</button>
+
+          {this.state.studentsVisible ? <Aux><Students /></Aux> : null }
+          {this.state.teachersVisible ? <Teachers /> : null }
+          {this.state.resourcesVisible ? <Resources /> : null }
+          {this.state.lessonsVisible ? <Lessons /> : null }
+
+
+
         </div>
       </Aux>
     );
