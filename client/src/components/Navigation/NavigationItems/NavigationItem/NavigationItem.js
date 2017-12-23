de149c576beef12e7f3384e2c0c6b073b25544c3
smithwebtek <brad@smithwebtek.com>
2017-12-22 20:16:36 -0500

    add Link, to prevent DOM reloads

--- client/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ client/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import classes from './NavigationItem.css';
+import { Route, Link } from 'react-router-dom';
 
 const navigationItem = (props) => (
   <li className={classes.NavigationItem}>
-    <a href={props.link} className={props.active ? classes.active : null}>{props.children}</a>
+    <Link to={{pathname: props.link}} className={props.active ? classes.active : null}>{props.children}</Link>
   </li>
 )
 
