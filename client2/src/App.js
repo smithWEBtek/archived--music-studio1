ecca0528364a5c2db18ac4b716f4a30b2dfe766b
smithwebtek <brad@smithwebtek.com>
2018-01-11 17:42:53 -0500

    migrate basic client config, reactstrap and run dependency installs

--- client2/src/App.js
+++ client2/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import Nav from './UI/Nav/Nav'
 
 class App extends Component {
   render() {
     return (
       <div>
-
+        <Nav />
       </div>
     );
   }
