b22b6b0e740d6c38afb0f18045e48b296a9cc510
smithwebtek <brad@smithwebtek.com>
2018-01-11 16:49:00 -0500

    add new client

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
