b0f31c3cfbc9e6b3b7b8ba3f025fe0f6f584183e
smithwebtek <brad@smithwebtek.com>
2018-01-12 10:21:50 -0500

    elaborating the studentslist table

--- client2/src/containers/Students/StudentsList/StudentsList.js
+++ client2/src/containers/Students/StudentsList/StudentsList.js
@@ -1,20 +1,44 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Aux from '../../../hoc/Aux/Aux'
+import { Table } from 'reactstrap'
+
 
 const StudentsList = ({ match, students }) => {
   console.log('[StudentsList] students', students)
 
   const renderStudents = students.map((student, index) => {
     return (
-      <Link
-        to={`/students/${student.id}`}
-        style={{ marginRight: '12px' }}
-        key={student.id}
-      >{student.lastname}</Link >
+      <tr key={index}>
+        <th scope="row">{student.id}</th>
+        <td><Link to={`/students/${student.id}`}
+          style={{ marginRight: '12px' }}
+          key={student.id}>{student.lastname}</Link></td>
+
+        <td>{student.teacher.lastname}</td>
+        <td>{student.level}</td>
+        <td>{student.lessons.length > 0 ? student.lessons[student.lessons.length - 1].date : 'no lessons yet'}</td>
+      </tr>
     )
-  }
-  );
+  })
+
+  return (
+    <Table striped>
+      <thead>
+        <tr>
+          <th>ID</th>
+          {/* <th>Firstname</th> */}
+          <th>Lastname</th>
+          <th>Teacher</th>
+          <th>Level</th>
+          <th>Last lesson date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {renderStudents}
+      </tbody>
+    </Table>
+  )
 
   return (
     <Aux>
