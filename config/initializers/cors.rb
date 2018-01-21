855276c563b9d1b35766290510de592b235680f1
smithwebtek <brad@smithwebtek.com>
2018-01-21 00:48:31 -0500

    edited cors file to include https://music-studio.herokuapp.com/

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', '127.0.0.1:3000'
+    origins 'localhost:3000', '127.0.0.1:3000', 'https://music-studio.herokuapp.com/'
 
     resource '*',
       headers: :any,
