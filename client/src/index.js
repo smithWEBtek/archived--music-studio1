aa697d067fc5c25942c9b7c52d93fb217b439d37
smithwebtek <brad@smithwebtek.com>
2018-02-13 00:04:10 -0500

    nope that wasn't it either

--- client/src/index.js
+++ client/src/index.js
@@ -36,7 +36,7 @@ const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, 
 
 const app = (
 	<Provider store={store}>
-		<BrowserRouter basename='/'>
+		<BrowserRouter>
 			<App className={classes.Body} />
 		</BrowserRouter>
 	</Provider>
